0319f22506f657613303b664e6ec003fbf4f2043
Kevin Bridges <kevin@cyberswat.com>
2021-01-27 15:51:28 -0700

    End of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, useLocation } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard'
 import EventForm from '../../features/events/eventDashboard/eventForm/EventForm'
 import NavBar from '../../features/nav/NavBar'
 import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage'
 import HomePage from '../../features/home/HomePage'
+import Sandbox from '../../features/sandbox/sandbox'
 
 function App() {
+  const { key } = useLocation()
   return (
     <>
       <Route exact path='/' component={HomePage} />
@@ -17,11 +19,13 @@ function App() {
           <>
             <NavBar />
             <Container className='main'>
+              <Route exact path='/sandbox' component={Sandbox} />
               <Route exact path='/events' component={EventDashboard} />
               <Route path='/events/:id' component={EventDetailedPage} />
               <Route
                 path={['/createEvent', '/manage/:id']}
                 component={EventForm}
+                key={key}
               />
             </Container>
           </>
