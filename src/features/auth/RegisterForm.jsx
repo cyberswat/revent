2b4a26c3e50493274d274735080914c19782586d
Kevin Bridges <kevin@cyberswat.com>
2021-02-01 14:24:25 -0700

    120 setting user profile data

--- src/features/auth/RegisterForm.jsx
+++ src/features/auth/RegisterForm.jsx
@@ -3,7 +3,7 @@ import ModalWrapper from '../../app/common/modals/modalWrapper'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import MyTextInput from '../../app/common/form/MyTextInput'
-import { Button } from 'semantic-ui-react'
+import { Button, Label } from 'semantic-ui-react'
 import { useDispatch } from 'react-redux'
 import { closeModal } from '../../app/common/modals/modalReducer'
 import { registerInFirebase } from '../../app/firestore/firebaseService'
@@ -23,18 +23,18 @@ export default function RegisterForm() {
           email: Yup.string().required().email(),
           password: Yup.string().required(),
         })}
-        onSubmit={async (values, { setSubmitting }) => {
+        onSubmit={async (values, { setSubmitting, setErrors }) => {
           try {
             await registerInFirebase(values)
             setSubmitting(false)
             dispatch(closeModal())
           } catch (error) {
+            setErrors({ auth: error.message })
             setSubmitting(false)
-            console.log(error)
           }
         }}
       >
-        {({ isSubmitting, isValid, dirty }) => (
+        {({ isSubmitting, isValid, dirty, errors }) => (
           <Form className='ui form'>
             <MyTextInput name='displayName' placeholder='Display Name' />
             <MyTextInput name='email' placeholder='Email Address' />
@@ -43,6 +43,14 @@ export default function RegisterForm() {
               placeholder='Password'
               type='password'
             />
+            {errors.auth && (
+              <Label
+                basic
+                color='red'
+                style={{ marginBottom: 10 }}
+                content={errors.auth}
+              />
+            )}
             <Button
               loading={isSubmitting}
               disabled={!isValid || !dirty || isSubmitting}
