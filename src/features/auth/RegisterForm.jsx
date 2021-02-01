faba4b03d2cad0a95d0b1cfba7a847333e76b010
Kevin Bridges <kevin@cyberswat.com>
2021-02-01 13:23:08 -0700

    end of lesson 118

--- src/features/auth/RegisterForm.jsx
+++ src/features/auth/RegisterForm.jsx
@@ -6,24 +6,26 @@ import MyTextInput from '../../app/common/form/MyTextInput'
 import { Button } from 'semantic-ui-react'
 import { useDispatch } from 'react-redux'
 import { closeModal } from '../../app/common/modals/modalReducer'
-import { signInWithEmail } from '../../app/firestore/firebaseService'
+import { registerInFirebase } from '../../app/firestore/firebaseService'
 
-export default function LoginForm() {
+export default function RegisterForm() {
   const dispatch = useDispatch()
   return (
-    <ModalWrapper size='mini' header='Sign in to Re-vents'>
+    <ModalWrapper size='mini' header='Register to Re-vents'>
       <Formik
         initialValues={{
+          displayName: '',
           email: '',
           password: '',
         }}
         validationSchema={Yup.object({
+          displayName: Yup.string().required(),
           email: Yup.string().required().email(),
           password: Yup.string().required(),
         })}
         onSubmit={async (values, { setSubmitting }) => {
           try {
-            await signInWithEmail(values)
+            await registerInFirebase(values)
             setSubmitting(false)
             dispatch(closeModal())
           } catch (error) {
@@ -34,6 +36,7 @@ export default function LoginForm() {
       >
         {({ isSubmitting, isValid, dirty }) => (
           <Form className='ui form'>
+            <MyTextInput name='displayName' placeholder='Display Name' />
             <MyTextInput name='email' placeholder='Email Address' />
             <MyTextInput
               name='password'
@@ -47,7 +50,7 @@ export default function LoginForm() {
               fluid
               size='large'
               color='teal'
-              content='Login'
+              content='Register'
             />
           </Form>
         )}
