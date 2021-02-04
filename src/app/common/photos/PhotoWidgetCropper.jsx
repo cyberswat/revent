e34904c5d1a105c1cd8ffd17681a28e6b268c591
Kevin Bridges <kevin@cyberswat.com>
2021-02-04 06:36:21 -0700

    145. React cropper

---
+++ src/app/common/photos/PhotoWidgetCropper.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Cropper from 'react-cropper'
+import 'cropperjs/dist/cropper.css'
+
+export default function PhotoWidgetCropper({ setImage, imagePreview }) {
+  let cropper
+
+  const onCropperInit = (component) => {
+    cropper = component
+  }
+
+  function cropImage() {
+    if (cropper && typeof cropper.getCroppedCanvas() === 'undefined') {
+      return
+    }
+    cropper.getCroppedCanvas().toBlob((blob) => {
+      setImage(blob)
+    }, 'image/jpeg')
+  }
+
+  return (
+    <Cropper
+      ref={cropper}
+      src={imagePreview}
+      style={{ height: 200, width: '100%' }}
+      // Cropper.js options
+      aspectRatio={1}
+      preview='.img-preview'
+      guides={false}
+      viewMode={1}
+      dragMode='move'
+      scalable={true}
+      cropBoxMovable={true}
+      cropBoxResizable={true}
+      crop={cropImage}
+      onInitialized={onCropperInit}
+    />
+  )
+}
