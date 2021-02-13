e4e54d9e2d38b2f518d06c878f11e9864c1fed34
Kevin Bridges <kevin@cyberswat.com>
2021-02-13 12:13:57 -0700

    Showing the modal on click

--- src/features/events/eventDetailed/EventDetailedHeader.jsx
+++ src/features/events/eventDetailed/EventDetailedHeader.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Segment, Image, Item, Header, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import { Button, Header, Image, Item, Segment } from 'semantic-ui-react'
 import { format } from 'date-fns'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 import {
   addUserAttendance,
   cancelUserAttendance,
 } from '../../../app/firestore/firestoreService'
-import { doToast } from '../../../app/common/util/util'
+import { useSelector } from 'react-redux'
+import UnauthModal from '../../auth/UnauthModal'
 
 const eventImageStyle = {
   filter: 'brightness(30%)',
@@ -23,13 +26,15 @@ const eventImageTextStyle = {
 
 export default function EventDetailedHeader({ event, isHost, isGoing }) {
   const [loading, setLoading] = useState(false)
+  const { authenticated } = useSelector((state) => state.auth)
+  const [modalOpen, setModalOpen] = useState(false)
 
   async function handleUserJoinEvent() {
     setLoading(true)
     try {
       await addUserAttendance(event)
     } catch (error) {
-      doToast(error)
+      toast.error(error.message)
     } finally {
       setLoading(false)
     }
@@ -40,75 +45,82 @@ export default function EventDetailedHeader({ event, isHost, isGoing }) {
     try {
       await cancelUserAttendance(event)
     } catch (error) {
-      doToast(error)
+      toast.error(error.message)
     } finally {
       setLoading(false)
     }
   }
 
   return (
-    <Segment.Group>
-      <Segment basic attached='top' style={{ padding: '0' }}>
-        <Image
-          src={`/assets/categoryImages/${event.category}.jpg`}
-          fluid
-          style={eventImageStyle}
-        />
+    <>
+      {modalOpen && <UnauthModal setModalOpen={setModalOpen} />}
+      <Segment.Group>
+        <Segment basic attached='top' style={{ padding: '0' }}>
+          <Image
+            src={`/assets/categoryImages/${event.category}.jpg`}
+            fluid
+            style={eventImageStyle}
+          />
 
-        <Segment basic style={eventImageTextStyle}>
-          <Item.Group>
-            <Item>
-              <Item.Content>
-                <Header
-                  size='huge'
-                  content={event.title}
-                  style={{ color: 'white' }}
-                />
-                <p>{format(event.date, 'MMMM d, yyyy h:mm a')}</p>
-                <p>
-                  Hosted by{' '}
-                  <strong>
-                    <Link to={`/profile/${event.hostUid}`}>
-                      {event.hostedBy}
-                    </Link>{' '}
-                  </strong>
-                </p>
-              </Item.Content>
-            </Item>
-          </Item.Group>
+          <Segment basic style={eventImageTextStyle}>
+            <Item.Group>
+              <Item>
+                <Item.Content>
+                  <Header
+                    size='huge'
+                    content={event.title}
+                    style={{ color: 'white' }}
+                  />
+                  <p>{format(event.date, 'MMMM d, yyyy h:mm a')}</p>
+                  <p>
+                    Hosted by{' '}
+                    <strong>
+                      <Link to={`/profile/${event.hostUid}`}>
+                        {event.hostedBy}
+                      </Link>{' '}
+                    </strong>
+                  </p>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Segment>
         </Segment>
-      </Segment>
 
-      <Segment attached='bottom' clearing>
-        {!isHost && (
-          <>
-            {isGoing ? (
-              <Button onClick={handleUserLeaveEvent} loading={loading}>
-                Cancel My Place
-              </Button>
-            ) : (
-              <Button
-                onClick={handleUserJoinEvent}
-                loading={loading}
-                color='teal'
-              >
-                JOIN THIS EVENT
-              </Button>
-            )}
-          </>
-        )}
+        <Segment attached='bottom' clearing>
+          {!isHost && (
+            <>
+              {isGoing ? (
+                <Button onClick={handleUserLeaveEvent} loading={loading}>
+                  Cancel My Place
+                </Button>
+              ) : (
+                  <Button
+                    onClick={
+                      authenticated
+                        ? handleUserJoinEvent
+                        : () => setModalOpen(true)
+                    }
+                    loading={loading}
+                    color='teal'
+                  >
+                    JOIN THIS EVENT
+                  </Button>
+                )}
+            </>
+          )}
 
-        {isHost && (
-          <Button
-            as={Link}
-            to={`/manage/${event.id}`}
-            color='orange'
-            floated='right'
-          >
-            Manage Event
-          </Button>
-        )}
-      </Segment>
-    </Segment.Group>
+          {isHost && (
+            <Button
+              as={Link}
+              to={`/manage/${event.id}`}
+              color='orange'
+              floated='right'
+            >
+              Manage Event
+            </Button>
+          )}
+        </Segment>
+      </Segment.Group>
+    </>
   )
 }
